0667eb46b5727b01e8724b9267df1a0f8aae16f4
Pedro <pedroasilva38@icloud.com>
2025-10-05 18:14:01 -0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+function comprar() {
+    tipoIngresso = document.getElementById('tipo-ingresso').value
+    qtdComprada = document.getElementById('qtd').value
+
+    switch (tipoIngresso) {
+        case 'superior':
+            terminarCompra('qtd-superior', qtdComprada)
+            break
+            
+        case 'inferior':
+            terminarCompra('qtd-inferior', qtdComprada)
+            break
+            
+        case 'pista':
+            terminarCompra('qtd-pista', qtdComprada)
+            break
+
+    }
+    document.getElementById('qtd').value = ''
+}
+
+function terminarCompra(tipo, qtd) {
+    let qtdRestante = parseInt(document.getElementById(`${tipo}`).textContent)
+    const nomeTipo = tipo.split('-')[1]
+    
+    if (qtd > qtdRestante) {
+        alert(`Não há ingressos suficientes para compra na ${nomeTipo}`)
+        return
+    }
+
+    qtdRestante -= qtd
+    document.getElementById(`${tipo}`).textContent = qtdRestante
+}
